ae159ab61b74fae80856ae77864f932b47372657
BAEK GYEONGCHEOL <bkctwo@tukorea.ac.kr>
2025-10-26 03:22:28 +0900

    게임 기본 모드 함수 및 코드 구현

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pico2d import *
-
+import mode_select_mode
+import character_select_mode
+import main_play_mode
 
 def handle_events():
     global running, gameModeSelectMode, characterSelectMode, playGameMode
@@ -8,8 +10,9 @@ def handle_events():
     for event in event_list:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
 
 running = True              # 게임 루프
 gameModeSelectMode = True   # 게임 모드 선택(vs 2P, vs AI, quit)
@@ -21,18 +24,12 @@ while running:
     handle_events()
     # 게임 모드 선택(vs 2P, vs AI, quit)
     while gameModeSelectMode:
-        print('게임 모드 선택')
-        break
-        pass
+        cur_state = mode_select_mode.run()
     # 캐릭터 선택 모드
     while characterSelectMode:
-        print('캐릭터 선택 모드')
-        break
-        pass
+        cur_state = character_select_mode.run()
     # 게임 플레이 모드
     while playGameMode:
-        print('게임 플레이 모드')
-        break
-        pass
+        cur_state = main_play_mode.run()
     delay(0.01)
 close_canvas()

a61729e92b48033601c1fd5c5c5223814afc79be
BAEK GYEONGCHEOL <bkctwo@tukorea.ac.kr>
2025-11-08 18:42:12 +0900

    충돌 범위 collision_pair 구현

--- game_world.py
+++ game_world.py
@@ -48,3 +48,41 @@ def clear():
         layer.clear()
 
 
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    # 일단 사각형의 모든 방향을 비교해서 충돌이 아닌 경우를 먼저 걸러낸다.
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    # 충돌이 아닌 경우를 다 걸러냈으면 충돌한 것이다.
+    return True
+
+
+# collision_pairs 딕셔너리에는 충돌 검사가 필요한 객체 쌍에 대한 정보가 저장된다.
+collision_pairs = {}
+
+def add_collision_pair(group, a, b):
+    # 맨 처음에는 그룹이 없다.
+    if group not in collision_pairs:    # 처음 추가하는 그룹이면
+        collision_pairs[group] = [[], []]   # 해당 그룹을 만든다.
+    if a:   # a의 경우 1번째 리스트에 추가
+        collision_pairs[group][0].append(a)
+    if b:   # b의 경우 2번째 리스트에 추가
+        collision_pairs[group][1].append(b)
+
+
+def handle_collision():
+    for group, pairs in collision_pairs.items():
+        for a in pairs[0]:  # a 리스트의 모든 객체에 대해서
+            for b in pairs[1]:  # b 리스트의 모든 객체에 대해서
+                if collide(a, b):   # a와 b의 충돌 검사
+                    # 객체에게 충돌이 발생했음을 알려준다.
+                    # 객체 스스로가 충돌에 대한 처리를 하도록 한다.
+                    # group은 충돌 그룹의 이름을 알려준다.
+                    # 2번째 인자는 충돌한 상대 객체이다.
+                    a.handle_collision(group, b)
+                    b.handle_collision(group, a)

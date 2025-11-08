9aa07dcd2e147de2bd82287e1c1124521e85c373
BAEK GYEONGCHEOL <bkctwo@tukorea.ac.kr>
2025-11-08 19:34:26 +0900

    main 함수 gameworld와 framework에 맞게 수정

--- main.py
+++ main.py
@@ -1,41 +1,7 @@
 from pico2d import *
-import mode_select_mode
-import character_select_mode
-import main_play_mode
+import game_framework
+import mode_select_mode as start_mode
 
-def handle_events():
-    global running
-
-    event_list = get_events()
-    for event in event_list:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_ESCAPE:
-                running = False
-
-running = True              # 게임 루프 실행 변수
-game_mode_state = 'mode_select' # 현재 게임 모드 상태
-battle_mode = None
-
-open_canvas(1594, 894)
-
-# 게임 루프
-while running:
-    handle_events()
-    # 게임 모드 선택(vs 2P, vs AI, quit)
-    if game_mode_state == 'mode_select':
-        cur_state, battle_mode = mode_select_mode.run()
-    # 캐릭터 선택 모드
-    elif game_mode_state == 'character_select':
-        cur_state = character_select_mode.run(battle_mode)
-    # 게임 플레이 모드
-    elif game_mode_state == 'main_play':
-        cur_state = main_play_mode.run(battle_mode)
-
-    game_mode_state = cur_state
-    if game_mode_state == 'QUIT':
-        running = False
-
-    delay(0.01)
+open_canvas()
+game_framework.run(start_mode)
 close_canvas()

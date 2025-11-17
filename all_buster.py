4e67a0ac7fa053df6c8f6702b8fb05b7b62ddb91
BAEK GYEONGCHEOL <bkctwo@tukorea.ac.kr>
2025-11-17 20:30:29 +0900

    투사체는 충돌 시 삭제!

--- all_buster.py
+++ all_buster.py
@@ -59,25 +59,9 @@ class NormalBuster:
     def get_attack_damage(self):
         return self.thrower.get_wave_damage(type(self))
 
-
-    # def handle_collision(self, group, other):
-    #     # group이 소년과 볼 사이의 충돌이라면
-    #     if group == 'boy:ball':
-    #         # 아래처럼 작성하면 지울 수 없는 에러가 발생!
-    #         # game_world에서는 ball 이 사라졌지만 collision_pairs 에는 여전히 남아 있기 때문이다.
-    #         # collision_pairs 에서는 계속해서 비교를 시도하게 되고, 이미 지워진 ball 객체에 접근하려고 하면서 에러가 발생한다.
-    #         # 해결 방법은 collision_pairs 에서도 해당 객체를 제거해 주어야 한다.
-    #         # 이 작업은 game_world.py 의 game_world.remove_object() 함수에서 처리해 준다.
-    #         game_world.remove_object(self)
-    #     # group이 잔디와 볼 사이의 충돌이라면
-    #     elif group == 'grass:ball' and self.stop_state == False:
-    #         # 볼이 잔디 위에서 멈춘다.
-    #         self.stopped = True
-    #         self.stop_state = True
-    #     elif group == 'zombie:ball' and self.stop_state == False:
-    #         # 볼이 좀비와 충돌하면 제거한다.
-    #         game_world.remove_object(self)
-
+    def handle_collision(self, group, other):
+        # 투사체는 충돌 시 사라지도록 삭제!
+        game_world.remove_object(self)
 
 class PowerBuster:
     image = None
@@ -132,23 +116,9 @@ class PowerBuster:
     def get_attack_damage(self):
         return self.thrower.get_wave_damage(type(self))
 
-    # def handle_collision(self, group, other):
-    #     # group이 소년과 볼 사이의 충돌이라면
-    #     if group == 'boy:ball':
-    #         # 아래처럼 작성하면 지울 수 없는 에러가 발생!
-    #         # game_world에서는 ball 이 사라졌지만 collision_pairs 에는 여전히 남아 있기 때문이다.
-    #         # collision_pairs 에서는 계속해서 비교를 시도하게 되고, 이미 지워진 ball 객체에 접근하려고 하면서 에러가 발생한다.
-    #         # 해결 방법은 collision_pairs 에서도 해당 객체를 제거해 주어야 한다.
-    #         # 이 작업은 game_world.py 의 game_world.remove_object() 함수에서 처리해 준다.
-    #         game_world.remove_object(self)
-    #     # group이 잔디와 볼 사이의 충돌이라면
-    #     elif group == 'grass:ball' and self.stop_state == False:
-    #         # 볼이 잔디 위에서 멈춘다.
-    #         self.stopped = True
-    #         self.stop_state = True
-    #     elif group == 'zombie:ball' and self.stop_state == False:
-    #         # 볼이 좀비와 충돌하면 제거한다.
-    #         game_world.remove_object(self)
+    def handle_collision(self, group, other):
+        # 투사체는 충돌 시 사라지도록 삭제!
+        game_world.remove_object(self)
 
 class Sphere:
     image = None
@@ -203,23 +173,9 @@ class Sphere:
     def get_attack_damage(self):
         return self.thrower.get_wave_damage(type(self))
 
-    # def handle_collision(self, group, other):
-    #     # group이 소년과 볼 사이의 충돌이라면
-    #     if group == 'boy:ball':
-    #         # 아래처럼 작성하면 지울 수 없는 에러가 발생!
-    #         # game_world에서는 ball 이 사라졌지만 collision_pairs 에는 여전히 남아 있기 때문이다.
-    #         # collision_pairs 에서는 계속해서 비교를 시도하게 되고, 이미 지워진 ball 객체에 접근하려고 하면서 에러가 발생한다.
-    #         # 해결 방법은 collision_pairs 에서도 해당 객체를 제거해 주어야 한다.
-    #         # 이 작업은 game_world.py 의 game_world.remove_object() 함수에서 처리해 준다.
-    #         game_world.remove_object(self)
-    #     # group이 잔디와 볼 사이의 충돌이라면
-    #     elif group == 'grass:ball' and self.stop_state == False:
-    #         # 볼이 잔디 위에서 멈춘다.
-    #         self.stopped = True
-    #         self.stop_state = True
-    #     elif group == 'zombie:ball' and self.stop_state == False:
-    #         # 볼이 좀비와 충돌하면 제거한다.
-    #         game_world.remove_object(self)
+    def handle_collision(self, group, other):
+        # 투사체는 충돌 시 사라지도록 삭제!
+        game_world.remove_object(self)
 
 
 class Wave:
@@ -275,20 +231,6 @@ class Wave:
     def get_attack_damage(self):
         return self.thrower.get_wave_damage(type(self))
 
-    # def handle_collision(self, group, other):
-    #     # group이 소년과 볼 사이의 충돌이라면
-    #     if group == 'boy:ball':
-    #         # 아래처럼 작성하면 지울 수 없는 에러가 발생!
-    #         # game_world에서는 ball 이 사라졌지만 collision_pairs 에는 여전히 남아 있기 때문이다.
-    #         # collision_pairs 에서는 계속해서 비교를 시도하게 되고, 이미 지워진 ball 객체에 접근하려고 하면서 에러가 발생한다.
-    #         # 해결 방법은 collision_pairs 에서도 해당 객체를 제거해 주어야 한다.
-    #         # 이 작업은 game_world.py 의 game_world.remove_object() 함수에서 처리해 준다.
-    #         game_world.remove_object(self)
-    #     # group이 잔디와 볼 사이의 충돌이라면
-    #     elif group == 'grass:ball' and self.stop_state == False:
-    #         # 볼이 잔디 위에서 멈춘다.
-    #         self.stopped = True
-    #         self.stop_state = True
-    #     elif group == 'zombie:ball' and self.stop_state == False:
-    #         # 볼이 좀비와 충돌하면 제거한다.
-    #         game_world.remove_object(self)
+    def handle_collision(self, group, other):
+        # 투사체는 충돌 시 사라지도록 삭제!
+        game_world.remove_object(self)

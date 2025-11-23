0e372fcbcf26903f62bbeec1b3b13ae1854b1e5c
BAEK GYEONGCHEOL <bkctwo@tukorea.ac.kr>
2025-11-23 22:29:16 +0900

    hp 클래스 파일 생성!

---
+++ hp.py
@@ -0,0 +1,28 @@
+from pico2d import *
+
+# hp 틀
+class HPTool:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = load_image('hp_tool.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(x, y)
+
+# hp 바
+class HPBar:
+    def __init__(self, character, x, y):
+        self.character = character     # 해당 캐릭터 체력바!
+        self.x = x
+        self.y = y
+        self.image = load_image('hp_bar.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(x, y)
